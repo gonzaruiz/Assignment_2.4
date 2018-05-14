95ddb117903cceee49d90ba2bcd2b5e3a5b39fd8
gonzalo ruiz <38412697+gonzaruiz@users.noreply.github.com>
2018-05-14 11:09:34 -0500

    Add files via upload

---
+++ Assignment_2.4.py
@@ -0,0 +1,8 @@
+
+# coding: utf-8
+
+# In[11]:
+
+
+print("    WE, THE PEOPLE OF INDIA, \n\t  having solemnly resolved to constitute india into a SOVEREIGN,! \n\t\tSOCIALIST, SECULAR, DEMOCRATIC REPUBLIC, \n\t\t and to secure to all its citizens: ")
+
